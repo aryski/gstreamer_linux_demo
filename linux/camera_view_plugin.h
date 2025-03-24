e4bdc92981a5ff611ad48d6e7f3b564e5fff5a47
Adam <ryski.a@appotype.eu>
2025-03-24 14:12:32 +0100

    5

--- linux/camera_view_plugin.h
+++ linux/camera_view_plugin.h
@@ -1,1 +1,8 @@
- 
+#ifndef CAMERA_VIEW_PLUGIN_H
+#define CAMERA_VIEW_PLUGIN_H
+
+#include <flutter_linux/flutter_linux.h>
+
+void camera_view_plugin_register_with_registrar(FlPluginRegistrar* registrar);
+
+#endif // CAMERA_VIEW_PLUGIN_H 
--- linux/generated_plugin_registrant.cc
+++ linux/generated_plugin_registrant.cc
@@ -2,15 +2,14 @@
 #include <flutter_linux/flutter_linux.h>
 #include <flutter_registrar.h>
 #include <url_launcher_linux/url_launcher_plugin.h>
-#include <camera_view/camera_view_plugin.h>
+#include "camera_view_plugin.h"
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) url_launcher_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
   url_launcher_plugin_register_with_registrar(url_launcher_linux_registrar);
   
-  // Register our camera view plugin
   g_autoptr(FlPluginRegistrar) camera_view_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "CameraView");
-  camera_view_register_with_registrar(camera_view_registrar);
+      fl_plugin_registry_get_registrar_for_plugin(registry, "CameraViewPlugin");
+  camera_view_plugin_register_with_registrar(camera_view_registrar);
 } 
