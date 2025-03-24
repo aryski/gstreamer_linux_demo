4a7b5830c1dc769cef3522240c338379fd12fe2e
Adam <ryski.a@appotype.eu>
2025-03-24 14:17:12 +0100

    eh

--- linux/camera_view_plugin.cc
+++ linux/camera_view_plugin.cc
@@ -1,6 +1,10 @@
 #include "camera_view_plugin.h"
 #include "camera_view.h"
 
-void camera_view_plugin_register_with_registrar(FlPluginRegistrar* registrar) {
+static void camera_view_plugin_register_with_registrar(FlPluginRegistrar* registrar) {
   camera_view_register_with_registrar(registrar);
+}
+
+void camera_view_plugin_register_with_registrar(FlPluginRegistrar* registrar) {
+  camera_view_plugin_register_with_registrar(registrar);
 } 
